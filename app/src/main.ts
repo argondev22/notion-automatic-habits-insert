d64aa2c71590f80c6deb1787d4315e7674799c9f
argon-dev22 <hyouga.programing@gmail.com>
2025-08-26 12:41:21 +0000

    feat: habitsデータベースから習慣を取得するfetchHabitsDatabase関数と、習慣をTodoデータベースに挿入するinsertHabitsToTodosDatabase関数をdomain.tsに追加し、main.tsでこれらの関数を呼び出すように変更しました。

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,17 +1,13 @@
-import { notionClient } from "./lib/notionhq/init"
+import { fetchHabitsDatabase, insertHabitsToTodosDatabase } from "./domain"
 
-async function main(): Promise<void> {
-	try {
-		const response = await notionClient.search({
-			filter: { value: 'page', property: 'object' },
-			page_size: 1
-		})
-		console.log('Notion connected. First result summary:')
-		console.log({ results: response.results })
-	} catch (error) {
-		console.error('Notion API call error:', error)
-		return
-	}
+async function main() {
+  try {
+    const habits = await fetchHabitsDatabase()
+    console.log(habits)
+    // await insertHabitsToTodosDatabase(habits)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 main()
