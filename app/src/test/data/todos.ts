41c16632873aa027e43fef40cd18acc1b2d876fb
argondev22 <hyoga.js@gmail.com>
2025-10-01 07:54:10 +0900

    feat: HabitおよびTodoインターフェースのプロファイルプロパティ名を`profile`から`profiles`に変更し、Todoの時間設定を関数化しました。

--- app/src/test/data/todos.ts
+++ app/src/test/data/todos.ts
@@ -2,19 +2,24 @@ import { Profile, Todo } from "../../domain/model";
 
 const now = new Date();
 
+function createDate(hour: number, minute: number): Date {
+  const tomorrow = now.getDate() + 1;
+  return new Date(now.getFullYear(), now.getMonth(), tomorrow, hour, minute, 0);
+}
+
 export const todos: Todo[] = [
   {
     name: "朝ごはん/ケア",
-    startTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 15, 0),
-    endTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 9, 0, 0),
-    profile: [Profile.PRIVATE],
+    startTime: createDate(8, 15),
+    endTime: createDate(9, 0),
+    profiles: [Profile.PRIVATE],
     content: [],
   },
   {
     name: "ランニング",
-    startTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 0, 0),
-    endTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 30, 0),
-    profile: [Profile.PRIVATE],
+    startTime: createDate(19, 0),
+    endTime: createDate(19, 30),
+    profiles: [Profile.PRIVATE],
     content: [],
   },
 ];
