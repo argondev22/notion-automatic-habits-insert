11bc6ebe8466ec9ac249cfaff63b689c6e59e137
argondev22 <hyoga.js@gmail.com>
2025-10-01 07:00:45 +0900

    feat: HabitおよびTodoインターフェースを追加し、プロファイル情報を含むタスクの構造を定義しました。

--- app/src/domain/model.ts
+++ app/src/domain/model.ts
@@ -1,5 +1,21 @@
 import { BlockObjectResponse } from "../lib/notionhq/type";
 
+export interface Habit {
+  name: string;
+  time: string;
+  days: Array<Day>;
+  profile: Array<Profile>;
+  content: BlockObjectResponse;
+}
+
+export interface Todo {
+  name: string;
+  startTime: string;
+  endTime: string;
+  profile: Array<Profile>;
+  content: BlockObjectResponse;
+}
+
 export enum Day {
   MONDAY,
   TUESDAY,
@@ -14,20 +30,4 @@ export enum Profile {
   ENGINEER,
   WORK,
   PRIVATE,
-}
-
-export interface Habit {
-  name: string;
-  time: string;
-  days: Array<Day>;
-  profile: Array<Profile>;
-  content: BlockObjectResponse;
-}
-
-export interface Todo {
-  name: string;
-  startTime: string;
-  endTime: string;
-  profile: Array<Profile>;
-  content: string;
 }
