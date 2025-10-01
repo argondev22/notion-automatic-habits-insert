c783e4a7bf4ec3e7cf818cc0659bef71f8bf8909
argondev22 <hyoga.js@gmail.com>
2025-10-02 07:37:17 +0900

    ref: ハビットをTodoデータベースに挿入する処理から不要なコメントを削除し、データ変換のコメントを整理しました。

--- app/src/domain/insert/insert.ts
+++ app/src/domain/insert/insert.ts
@@ -6,15 +6,15 @@ import { todos } from "../../test/data/todos";
 export async function insertHabitsToTodosDatabase(
   habits: Habit[]
 ): Promise<void> {
+  // DB_HABITSのデータ形式をDB_TODOSのデータ形式に変換
+  // const todos = convertHabitToTodos(habits);
+
   // DB_TODOSのIDを取得
   const todosDatabaseId = getTodosDatabaseId();
   if (!todosDatabaseId) {
     console.log("データベースIDが指定されていません");
     return;
   }
-
-  // DB_HABITSのデータ形式をDB_TODOSのデータ形式に変換
-  // const todos = convertHabitToTodos(habits);
 
   // DB_TODOSにデータを追加
   await insertTodosToDatabase(todosDatabaseId, todos);
