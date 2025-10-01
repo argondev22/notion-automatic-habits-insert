3b58cc4396952e6529d6e7d17f25de11f54dc3ea
argondev22 <hyoga.js@gmail.com>
2025-10-02 07:28:16 +0900

    feat: 不要な関数を削除し、Todoデータベースへのハビット挿入処理を整理しました。また、データベースID取得関数とTodo挿入関数を新たに追加しました。

--- app/src/domain/insert/insert.ts
+++ app/src/domain/insert/insert.ts
@@ -1,5 +1,6 @@
-import { notionClient } from "../../lib/notionhq/init";
-import { Habit, Todo, Profile } from "../model";
+import { Habit } from "../model";
+import { getTodosDatabaseId } from "./getTodosDatabaseId";
+import { insertTodosToDatabase } from "./insertTodosToDatabase";
 import { todos } from "../../test/data/todos";
 
 export async function insertHabitsToTodosDatabase(
@@ -17,63 +18,4 @@ export async function insertHabitsToTodosDatabase(
 
   // DB_TODOSにデータを追加
   await insertTodosToDatabase(todosDatabaseId, todos);
-}
-
-function getTodosDatabaseId() {
-  const todosDatabaseId = process.env.TODOS_DATABASE_ID;
-  if (!todosDatabaseId) {
-    return;
-  }
-  return todosDatabaseId;
-}
-
-function convertHabitToTodos(habits: Habit[]): Todo[] {
-  return [];
-}
-
-async function insertTodosToDatabase(todosDatabaseId: string, todos: Todo[]): Promise<void> {
-  for (const todo of todos) {
-    await insertTodoToDatabase(todosDatabaseId, todo);
-    console.log(
-      `Todoを作成しました: ${todo.name}`
-    );
-  }
-}
-
-async function insertTodoToDatabase(
-  databaseId: string,
-  todo: Todo
-): Promise<void> {
-  const profilePageId = {
-    [Profile.PRIVATE]: process.env.PRIVATE_PROFILE_ID,
-    [Profile.ENGINEER]: process.env.ENGINEER_PROFILE_ID,
-    [Profile.WORK]: process.env.WORK_PROFILE_ID,
-  };
-  const profilePageIds = todo.profiles.map(p => profilePageId[p]);
-
-  const properties: any = {
-    NAME: {
-      title: [
-        {
-          text: {
-            content: todo.name,
-          },
-        },
-      ],
-    },
-    EXPECTED: {
-      date: {
-        start: todo.startTime.toISOString(),
-        end: todo.endTime.toISOString(),
-      },
-    },
-    PROFILE: {
-      relation: profilePageIds.map(id => ({ id })),
-    },
-  };
-
-  await notionClient.pages.create({
-    parent: { database_id: databaseId },
-    properties,
-  });
 }
