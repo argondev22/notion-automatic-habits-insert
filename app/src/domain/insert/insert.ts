6eb5272674975929b5d4a055c7947460d4106f44
argondev22 <hyoga.js@gmail.com>
2025-10-02 08:18:57 +0900

    feat: ハビットをTodoデータベースに挿入する処理にエラーハンドリングを追加し、データ変換のロジックを整理しました。

--- app/src/domain/insert/insert.ts
+++ app/src/domain/insert/insert.ts
@@ -1,21 +1,25 @@
 import { Habit } from "../model";
+import { convertHabitToTodos } from "./convertHabitToTodos";
 import { getTodosDatabaseId } from "./getTodosDatabaseId";
 import { insertTodosToDatabase } from "./insertTodosToDatabase";
-import { todos } from "../../test/data/todos";
 
 export async function insertHabitsToTodosDatabase(
   habits: Habit[]
 ): Promise<void> {
-  // DB_HABITSのデータ形式をDB_TODOSのデータ形式に変換
-  // const todos = convertHabitToTodos(habits);
+  try {
+    // DB_HABITSのデータ形式をDB_TODOSのデータ形式に変換
+    const todos = convertHabitToTodos(habits);
 
-  // DB_TODOSのIDを取得
-  const todosDatabaseId = getTodosDatabaseId();
-  if (!todosDatabaseId) {
-    console.log("データベースIDが指定されていません");
-    return;
+    // DB_TODOSのIDを取得
+    const todosDatabaseId = getTodosDatabaseId();
+    if (!todosDatabaseId) {
+      console.log("データベースIDが指定されていません");
+      return;
+    }
+
+    // DB_TODOSにデータを追加
+    await insertTodosToDatabase(todosDatabaseId, todos);
+  } catch (error) {
+    console.error(error);
   }
-
-  // DB_TODOSにデータを追加
-  await insertTodosToDatabase(todosDatabaseId, todos);
 }
