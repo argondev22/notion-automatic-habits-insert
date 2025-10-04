e4c5431667887fc6c6c0b0a35bfd987e70abf1c1
argondev22 <hyoga.js@gmail.com>
2025-10-05 08:07:20 +0900

    feat: ESLint v9対応と設定ファイルの移行
    - .eslintrc.jsonからeslint.config.jsへの移行
    - package.jsonにtype: moduleを追加
    - @eslint/js依存関係を追加
    - Node.js環境のグローバル変数設定を追加

---
+++ app/eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+        setTimeout: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      'no-unused-vars': 'off', // TypeScript版を使用
+      'no-undef': 'off', // TypeScriptが型チェックを行うため
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', '*.js'],
+  },
+];
